878fa1709149f86bc6b7125d40e6406fa1c0903b
Matt Carter <m@ttcarter.com>
2022-06-27 13:33:13 +1000

    task: Initial commit

---
+++ app.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import {Command} from 'commander';
+import 'commander-extras';
+import micromatch from 'micromatch';
+import {filterExistingProcesses} from 'process-exists';
+import psList from 'ps-list';
+import {taskkill} from 'taskkill';
+import {stringReplaceLast} from './lib/stringReplaceLast.js';
+import timestring from 'timestring';
+
+let args = new Command()
+	.usage('[-k]')
+	.option('-a, --all', 'Show all processes not just this users')
+	.option('-l, --list', 'Show all matching processes')
+	.option('-f, --force', 'Force kill processes (implies --kill)')
+	.option('-k, --kill', 'Attempt to kill found processes')
+	.option('-n, --name', 'Match the name of the command only instead of args')
+	.option('-v, --verbose', 'Be verbose about what is happening')
+	.option('-w, --wait <time>', 'Wait for a valid timestring before zapping', '3s')
+	.option('-z, --zap', 'Try to politely kill a process then agressively (implies --kill)')
+	.option('--no-case', 'Disable case insesitive searching')
+	.option('--no-surround', 'Disable adding globstars at the start and end of search strings')
+	.note('If --kill, --force, --zap and --list are omitted --list is assumed')
+	.parse(process.argv);
+
+args = {args: args.args, ...args.opts()}; // Splice `args` into args key
+
+Promise.resolve()
+	// Sanity checks {{{
+	.then(()=> {
+		if (!args.kill && !args.force && !args.zap && !args.list) args.list = true;
+		if (args.force && args.zap) throw new Error('Using --force and --zap together makes no sense');
+	})
+	// }}}
+	.then(()=> psList({all: args.all}))
+	.then(procs => {
+		let isMatch = micromatch.matcher(
+			args.args.map(a => args.surround ? `*${a}*` : a),
+			{
+				contains: true,
+				nocase: args.case,
+			},
+		);
+
+		return procs.filter(p => isMatch(args.name ? p.name : p.cmd));
+	})
+	.then(async (procs) => Promise.all(procs.map(proc => {
+		if (args.list) {
+			procs.forEach(proc => console.log(
+				chalk.blue.bold(proc.pid),
+				stringReplaceLast(proc.cmd, proc.name, v => chalk.bold.white(v)),
+			))
+		}
+
+		if (args.kill || args.zap) {
+			if (!args.list || args.verbose) console.log(
+				chalk.red.bold('Politely killing'),
+				chalk.blue.bold(proc.pid),
+				stringReplaceLast(proc.cmd, proc.name, v => chalk.bold.white(v)),
+			);
+
+			return taskkill(proc.pid);
+		}
+	})).then(()=> procs))
+	.then(async (procs) => {
+		if (!args.zap || !args.wait) return procs;
+		let parsedWait = timestring(args.wait, 'ms');
+		console.log('Waiting', args.wait, chalk.gray(`(${parsedWait}ms)`));
+		await new Promise(resolve => setTimeout(resolve, parsedWait));
+		return procs;
+	})
+	.then(procs => {
+		if (args.zap) { // If zapping remove processes that politely exited
+			return Promise.all(procs.map(proc => ({
+				...proc,
+				remains: processExists(proc.pid),
+			})))
+				.then(procs => procs.filter(proc => proc.remains))
+		}
+	})
+	.then(async (procs) => (args.zap || args.force) && Promise.all(procs.map(proc => {
+		if (!args.list || args.verbose) console.log(
+			chalk.red.bold.italic('Vilolently killing'),
+			chalk.blue.bold(proc.pid),
+			stringReplaceLast(proc.cmd, proc.name, v => chalk.bold.white(v)),
+		);
+
+		return taskkill(proc.pid, {force: true});
+	})))
+	.then(()=> process.exit(0))
+	.catch(e => {
+		console.warn(e.toString());
+		process.exit(1);
+	})
