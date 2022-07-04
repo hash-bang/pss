927ed1627530305f97eba25104155757a595f64c
Matt Carter <m@ttcarter.com>
2022-07-04 11:50:00 +1000

    feat: Can now sudo automatically (if possible) + feat: Can now search by specific PIDs OR ports

--- app.js
+++ app.js
@@ -3,12 +3,13 @@
 import chalk from 'chalk';
 import {Command} from 'commander';
 import 'commander-extras';
+import {execa} from 'execa';
 import fuzzy from 'fuzzy';
 import inquirer from 'inquirer';
 import inquirerCheckboxPlusPrompt from 'inquirer-checkbox-plus-prompt';
 import micromatch from 'micromatch';
 import {filterExistingProcesses} from 'process-exists';
-// import pidToPort from 'pid-port';
+import {portToPid} from 'pid-port';
 import psList from 'ps-list';
 import fkill from 'fkill';
 import {stringReplaceLast} from './lib/stringReplaceLast.js';
@@ -25,14 +26,18 @@ let args = new Command()
 	.option('-f, --force', 'Force kill processes (implies --kill)')
 	.option('-k, --kill', 'Attempt to kill found processes')
 	.option('-n, --name', 'Match the name of the command only instead of args')
+	.option('-p, --pid <pids...>', 'List of specific Process IDs to limit to')
 	.option('-v, --verbose', 'Be verbose about what is happening')
 	.option('-w, --wait <time>', 'Wait for a valid timestring before zapping', '3s')
 	.option('-z, --zap', 'Try to politely kill a process then agressively (implies --kill)')
 	.option('--no-case', 'Disable case insesitive searching')
+	.option('--no-sudo', 'Do not try to elevate this process to sudo if possible')
 	.option('--no-skip-self', 'Exclude the PSS process from the list')
 	.option('--no-surround', 'Disable adding globstars at the start and end of search strings')
 	.option('--no-tree', 'Dont attempt to kill all sub-processes on --force / --zap')
+	.note('Globs can be any valid string, if the string is numeric and begins with `:` its assumed to be a port e.g. `:80` fetches the process listening on port 80')
 	.note('If --kill, --force, --zap and --list are omitted --list is assumed')
+	.note('--pid Can take a CSV of PIDs to filter by')
 	.parse(process.argv);
 
 args = {args: args.args, ...args.opts()}; // Splice `args` into args key
@@ -43,20 +48,57 @@ Promise.resolve()
 		if (!args.kill && !args.force && !args.zap && !args.list) args.list = true;
 		if (args.force && args.zap) throw new Error('Using --force and --zap together makes no sense');
 		if (!args.args.length) [args.args, args.surround] = [['*'], false];
+
+		// Make sure args.pid is always a set
+		args.pid = new Set(args.pid ? args.pid.split(/\s*,\s*/) : []);
 	})
 	// }}}
-	.then(()=> psList({all: args.all}))
-	.then(procs => {
-		let isMatch = micromatch.matcher(
-			args.args.map(a => args.surround ? `*${a}*` : a),
-			{
+	// Self sudo {{{
+	.then(()=> args.sudo && process.getuid() !== 0 && Promise.resolve()
+		.then(()=> execa('sudo', ['--non-interactive', 'echo'])
+			.catch(()=> {
+				console.warn('Sudo not available - limited to user owned processes only');
+				throw 'NEXT';
+			})
+		)
+		.then(()=> execa('sudo', process.argv, {
+			stdio: 'inherit',
+		}))
+		.then(()=> { throw 'EXIT' })
+		.catch(e => { if (e !== 'NEXT') throw e })
+	)
+	// }}}
+	.then(()=> Promise.all([
+		// Fetch all processes
+		psList({all: args.all}),
+
+		// Find any specific port listeners (e.g. `:80 :1234`)
+		...args.args
+			.filter(a => a.startsWith(':') && isFinite(a.substr(1)))
+			.map(a => +a.substr(1))
+			.map(port => portToPid(port)
+				.then(pid => {
+					console.warn('Got PID', pid, 'listening on port', port);
+					args.pid.add(pid);
+				})
+				.catch(()=> console.warn('Cannot find process listening on port', port))
+			),
+	]))
+	.then(([procs]) => {
+		let globs = args.args
+			.filter(a => !a.startsWith(':') || !isFinite(a.substr(1)))
+			.map(a => args.surround ? `*${a}*` : a);
+
+		let isMatch = globs.length > 0
+			? micromatch.matcher(globs, {
 				contains: true,
 				nocase: args.case,
-			},
-		);
+			})
+			: ()=> true;
 
 		return procs.filter(p =>
 			(!args.skipSelf || p.pid !== process.pid)
+			&& (!args.pid || args.pid.has(p.pid))
 			&& isMatch(args.name ? p.name : p.cmd)
 		);
 	})
@@ -123,6 +165,10 @@ Promise.resolve()
 	})))
 	.then(()=> process.exit(0))
 	.catch(e => {
-		console.warn(e.toString());
-		process.exit(1);
+		if (e === 'EXIT') {
+			process.exit(0);
+		} else {
+			console.warn(e.toString());
+			process.exit(1);
+		}
 	})
